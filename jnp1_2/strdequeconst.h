dbceace69966028b3e45861f448bd739b8fce178
Kuba <kuba.pierewoj@gmail.com>
2016-10-27 11:26:26 -0400

    poprawki z maila od prowadzacej

--- jnp1_2/strdequeconst.h
+++ jnp1_2/strdequeconst.h
@@ -1,2 +1,16 @@
-unsigned long emptystrdeque();
+#ifndef STRDEQUECONST_H
+#define STRDEQUECONST_H
 
+#ifdef __cplusplus
+namespace jnp1 {
+	extern "C" {
+#endif
+
+		unsigned long emptystrdeque();
+
+#ifdef __cplusplus
+	}
+}
+#endif
+
+#endif /* STRDEQUECONST_H */
